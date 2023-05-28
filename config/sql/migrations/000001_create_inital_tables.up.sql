22066a71c6ec26cf218e7e4d8d580f6303ac6f26
emanuelvsz <usermane13@gmail.com>
2023-05-28 15:22:42 -0300

    creating account and role

--- config/sql/migrations/000001_create_inital_tables.up.sql
+++ config/sql/migrations/000001_create_inital_tables.up.sql
@@ -1,42 +1,25 @@
 create extension if not exists "uuid-ossp";
 
-create table if not exists user_account
+create table if not exists role
 (
-    id    uuid          not null
-        constraint pk_user_account_id primary key,
-    pwd   text          not null,
-    hash  varchar(1024) not null,
-    email varchar(64)   not null
+    id      uuid            not null
+        constraint pk_role_id primary key,
+    name    varchar         not null,
+    code    varchar         not null
 );
 
-create table if not exists profile
+create table if not exists account
 (
     id          uuid        not null
-        constraint pk_profile_id primary key,
-    user_id     uuid        not null,
+        constraint pk_account_id primary key
+        constraint df_account_id default uuid_generate_v4(),
+    role_id     uuid        not null,
     name        varchar(64) not null,
-    picture_url varchar(64) null,
+    email       varchar(64) not null,
+    hash        varchar(64) not null,
+    password    text        not null
 
 
-    constraint fk_profile_user_account
-        foreign key (user_id) references user_account (id)
-);
-
--- Tabela principal para os artistas
-CREATE TABLE IF NOT EXISTS artist (
-    id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(64) NOT NULL
-);
-
--- Tabela para armazenar os gêneros
-CREATE TABLE IF NOT EXISTS genre (
-    id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(64) NOT NULL
-);
-
--- Tabela de junção entre artistas e gêneros
-CREATE TABLE IF NOT EXISTS artist_genre (
-    artist_id UUID NOT NULL REFERENCES artist (id),
-    genre_id UUID NOT NULL REFERENCES genre (id),
-    PRIMARY KEY (artist_id, genre_id)
+    constraint fk_account_role_id
+        foreign key (role_id) references role (id)
 );
