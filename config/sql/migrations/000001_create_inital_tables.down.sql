285f3558d83f5cb7a92b929adce8d18c98191455
emanuelvsz <usermane13@gmail.com>
2023-05-28 16:09:57 -0300

    final commit

--- config/sql/migrations/000001_create_inital_tables.down.sql
+++ config/sql/migrations/000001_create_inital_tables.down.sql
@@ -1,3 +1,13 @@
-drop table profile,
+drop table role,
+    account,
     artist,
-    user_account cascade;
+    album,
+    song,
+    song_artist,
+    followed_artist,
+    genre,
+    artist_genre,
+    song_genre,
+    album_genre;
+
+drop extension "uuid-ossp";
