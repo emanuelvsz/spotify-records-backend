b44244b03b27372bea74fccf8832f7653abbe930
emanuelvsz <usermane13@gmail.com>
2023-05-31 14:27:25 -0300

    comment handleError

--- src/infra/postgres/auth_postgres_repository.go
+++ src/infra/postgres/auth_postgres_repository.go
@@ -5,8 +5,6 @@ import (
 	"module/src/core/domain/user/credentials"
 	"module/src/core/errors"
 	"module/src/core/interfaces/repository"
-	"module/src/core/messages"
-	"strings"
 )
 
 var _ repository.AuthLoader = &AuthPostgresRepository{}
@@ -19,15 +17,15 @@ func (instance *AuthPostgresRepository) Login(credentials credentials.Credential
 	return nil, nil
 }
 
-func (instance AuthPostgresRepository) handleError(err error) errors.Error {
-	msg := err.Error()
+// func (instance AuthPostgresRepository) handleError(err error) errors.Error {
+// 	msg := err.Error()
 
-	if strings.Contains(msg, "sql: no rows in result set") {
-		return errors.NewNotFoundError(messages.InvalidCredentialsErrorMessage, err)
-	}
+// 	if strings.Contains(msg, "sql: no rows in result set") {
+// 		return errors.NewNotFoundError(messages.InvalidCredentialsErrorMessage, err)
+// 	}
 
-	return errors.NewUnexpectedError(messages.UnexpectedErrorMessage, err)
-}
+// 	return errors.NewUnexpectedError(messages.UnexpectedErrorMessage, err)
+// }
 
 func NewAuthPostgresRepository(manager connectorManager) *AuthPostgresRepository {
 	return &AuthPostgresRepository{manager}
